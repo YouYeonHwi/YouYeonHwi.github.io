5cd1f1fafd7677d873bb93651878f7f4b81128f7
YouYeonHwi <31821556+YouYeonHwi@users.noreply.github.com>
2026-04-29 10:28:35 +0900

    404 페이지 추가

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'devwhistle-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/404.html',
+  '/games/',
+  '/assets/gitbook/style.css',
+  '/assets/images/mascot_404.png'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
